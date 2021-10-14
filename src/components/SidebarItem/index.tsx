c2f4ee10bf81b366e22fcf59b9a1af43853e229b
Arthur Yan <arthuryan08@gmail.com>
2021-10-14 19:18:26 -0300

    Sidebar Items style

--- src/components/SidebarItem/index.tsx
+++ src/components/SidebarItem/index.tsx
@@ -1,4 +1,8 @@
+import { Link } from 'react-router-dom'
 import * as C from './styles'
+import { ReactComponent as ProfileIcon } from '../../svgs/profile.svg'
+import { ReactComponent as BookIcon } from '../../svgs/book.svg'
+import { ReactComponent as MailIcon } from '../../svgs/mail.svg'
 
 type Props = {
   title: string;
@@ -10,7 +14,24 @@ type Props = {
 export const SidebarItem = ({ title, description, icon, path }: Props) => {
   return (
     <C.Container>
-      ...
+      <Link to={path}>
+        <C.Info>
+          <C.Title>{title}</C.Title>
+          <C.Description>{description}</C.Description>
+        </C.Info>
+        <C.IconArea>
+          {icon === 'profile' &&
+            <ProfileIcon fill='white' width={24} height={24}/>
+          }
+          {icon === 'book' &&
+            <BookIcon fill='white' width={24} height={24}/>
+          }
+          {icon === 'mail' &&
+            <MailIcon fill='white' width={24} height={24}/>
+          }
+        </C.IconArea>
+        <C.Point></C.Point>
+      </Link>
     </C.Container>
   )
 }
