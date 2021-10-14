058c1b00e836d517c9b721fce51281e1d7d3cae3
Arthur Yan <arthuryan08@gmail.com>
2021-10-14 18:20:28 -0300

    step 2

--- src/components/SelectOption/styles.ts
+++ src/components/SelectOption/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const Container = styled.div<{ selected: boolean }>`
   display: flex;
-  border: 2px solid #16195C;
+  border: 2px solid ${props => props.selected ? '#25CD89' : '#16195C'};
   border-radius: 10px;
   padding: 20px;
   margin-bottom: 15px;
@@ -27,8 +27,16 @@ export const Icon = styled.div`
 
 export const Info = styled.div`
   flex: 1;
+  margin-left: 20px;
 `
 
-export const Title = styled.div``
+export const Title = styled.div`
+  font-size: 17px;
+  font-weight: bold;
+  margin-bottom: 7px;
+`
 
-export const Description = styled.div``
+export const Description = styled.div`
+  font-size: 14px;
+  color: #B8B8B4
+`
--- src/pages/FormStep2/index.tsx
+++ src/pages/FormStep2/index.tsx
@@ -1,4 +1,4 @@
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import * as C from "./styles";
 import { useForm, FormActions } from '../../contexts/FormContext'
 import { Theme } from "../../components/Theme";
@@ -10,10 +10,14 @@ export const FormStep2 = () => {
   const { state, dispatch } = useForm();
 
   useEffect(()=>{
-    dispatch({
-      type: FormActions.setCurrentStep,
-      payload: 2
-    })
+    if(state.name === '') {
+      history.push('/')
+    } else {
+      dispatch({
+        type: FormActions.setCurrentStep,
+        payload: 2
+      })
+    }
   }, [])
 
   const handleNextStep = () => {
@@ -24,13 +28,19 @@ export const FormStep2 = () => {
     }
   }
 
+  const setLevel = (level: number) => {
+    dispatch({
+      type: FormActions.setLevel,
+      payload: level
+    });
+  }
 
   return (
     <Theme>
       <C.Container>
         <p>Passo 2/3</p>
-        <h1>Vamos começar com o seu nome</h1>
-        <p>Preencha o campo a abaixo com o seu nome completo.</p>
+        <h1>{state.name}, o que melhor descreve você?</h1>
+        <p>Escolha a opção que melhor condiz com eu estado atual profissionalmente.</p>
         
         <hr/>
 
@@ -38,14 +48,19 @@ export const FormStep2 = () => {
           title='Sou iniciante'
           description='Comecei a programar há menos de 2 anos'
           icon="🥳"
+          selected={ state.level === 0 }
+          onClick={() => setLevel(0)}
         />
 
         <SelectOption 
           title='Sou programador'
           description='Já programo há 2 anos ou mais'
           icon="😎"
+          selected={ state.level === 1 }
+          onClick={() => setLevel(1)}
         />
-
+        
+        <Link to='/' className='backButton' >Voltar</Link>
         <button onClick={handleNextStep}>Próximo</button>
       </C.Container>
     </Theme>
